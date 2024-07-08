f745ce3aab18e14f0c9bce6e27e289bf6d6e7f99
Ahnabu <horairaabu013025@gmail.com>
2024-07-08 13:30:56 +0600

    dashboard sidebar and navbar finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 
 import './App.css'
+import Navbar from './Navbar'
+import Sidebar from './Sidebar'
 
 function App() {
  
 
   return (
-    <>
-      
-    </>
+    <div className='w-full flex mx-auto '>
+      <div className='w-[20vw]'>
+        <Sidebar/>
+      </div>
+      <div className='w-[80vw]'>
+         <Navbar/>
+      </div>
+    
+    </div>
   )
 }
 
